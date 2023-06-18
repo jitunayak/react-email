336641e7fd7961dad58d04fdfc7eddfbfc084524
Jitu Nayak <jitunayak715@gmail.com>
2023-06-18 14:50:25 +0530

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,41 @@
+import * as dotenv from 'dotenv'
+import { resolve } from 'path'
+
+const MISSING_ENV = 'Envrionemt is not set for'
+
+const env = process.env.NODE_ENV
+if (env === undefined) throw new Error(`${MISSING_ENV} NODE_ENV`)
+
+const path = configureEnvPath(env)
+dotenv.config({ path: path })
+
+export const CONFIG = {
+        MONGO_URI: process.env.MONGO_URI,
+        ENV: process.env.NODE_ENV,
+}
+
+validateEnvironment()
+
+function configureEnvPath(env: string): string | undefined {
+        switch (env) {
+                case 'dev': {
+                        const path = resolve(
+                                __dirname,
+                                `./../env/${process.env.NODE_ENV}.env`
+                        )
+                        return path
+                }
+                case 'prod': {
+                        return undefined // prod deployment will have system enviroment values
+                }
+                default:
+                        throw new Error('No Envrionemt is specified')
+        }
+}
+
+function validateEnvironment() {
+        Object.entries(CONFIG).map((key) => {
+                if (key[1] === undefined || key[1] === null)
+                        throw new Error(`${MISSING_ENV} ${key[0]}`) // throw error if any enviroment values are missing
+        })
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { CONFIG } from './config'
+
+console.log('Running Envrionemt:', CONFIG.ENV, CONFIG.MONGO_URI)
